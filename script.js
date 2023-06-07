56b16d246a2125d72a0d4a597eb1ea4a516e70db
Aloud <Aloud@tad.local>
2023-06-07 17:22:17 +0300

    removing filter and button when theres nothing

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearBtn = document.getElementById('clear');
-
+const itemFilter = document.getElementById('filter');
 function addItem(e) {
 
     e.preventDefault();
@@ -21,6 +21,10 @@ function addItem(e) {
     li.appendChild(button);
 
     itemList.appendChild(li);
+
+    checkUI();
+
+    itemInput.value = '';
 }
 
 function createButton(classes) {
@@ -39,7 +43,10 @@ function createIcon(classes) {
 
 function removeItem(e) {
     if (e.target.parentElement.classList.contains('remove-item')) {
-        e.target.parentElement.parentElement.remove();
+        if (confirm('Are you sure ?')) {     
+            e.target.parentElement.parentElement.remove();
+            checkUI();
+        }
     } 
 }
 
@@ -47,8 +54,23 @@ function removeAll(e) {
     while (itemList.firstChild) {
         itemList.removeChild(itemList.firstChild);
     }
+
+    checkUI();
+}
+
+function checkUI(){
+    const items = itemList.querySelectorAll('li');
+    if (items.length == 0) {
+        clearBtn.style.display = 'none';
+        itemFilter.style.display = 'none';
+    }else{
+        clearBtn.style.display = 'block';
+        itemFilter.style.display = 'block';
+    }
 }
 // Event Listeners 
 itemForm.addEventListener('submit',addItem);
 itemList.addEventListener('click',removeItem);
 clearBtn.addEventListener('click',removeAll);
+
+checkUI();
